df4c0c5a8eedc282ddb08061a71383a08b513970
Decal Bagel <3894957+DylanYasen@users.noreply.github.com>
2024-12-15 11:38:13 -0800

    basic card hover visuals

--- Assets/Scripts/UI/CardUI.cs
+++ Assets/Scripts/UI/CardUI.cs
@@ -1,14 +1,22 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class CardUI : MonoBehaviour
+public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public TMP_Text cardNameText;
     public TMP_Text descriptionText;
     public Image cardArtImage;
 
     private Card card;
+
+    private Vector3 originalScale;
+
+    private void Start()
+    {
+        originalScale = transform.localScale;
+    }
 
     public void Setup(Card cardData)
     {
@@ -25,5 +33,16 @@ public class CardUI : MonoBehaviour
     {
         Debug.Log($"Playing card: {card.cardName}");
         // Handle card logic here (apply effect, deduct cost, etc.)
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // @todo: smooth this out with tweening
+        transform.localScale = originalScale * 1.2f; // Enlarge card
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = originalScale; // Reset scale
     }
 }
