e2283633877123f367c3d149324d37725f4a2e51
Decal Bagel <3894957+DylanYasen@users.noreply.github.com>
2024-12-15 13:38:36 -0800

    slowing shaping card effect interface

--- Assets/Scripts/Card/Effect/AttackEffect.cs
+++ Assets/Scripts/Card/Effect/AttackEffect.cs
@@ -3,15 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AttackEffect", menuName = "Card Effects/Attack")]
 public class AttackEffect : CardEffect
 {
-    public override void Execute()
+    public override void Execute(Unit source, Unit target, int effectValue)
     {
-        // @todo: targeting
-        Enemy target = null;
-
         if (target != null)
         {
-            // target.TakeDamage(card.effectValue);
-            // Debug.Log($"{card.cardName} dealt {card.effectValue} damage to {target.enemyName}.");
+            CombatManager.Instance.ApplyDamage(source, target, effectValue);
         }
     }
 }
--- Assets/Scripts/Card/Effect/CardEffect.cs
+++ Assets/Scripts/Card/Effect/CardEffect.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public abstract class CardEffect : ScriptableObject
 {
-    public abstract void Execute();
+    public abstract void Execute(Unit source, Unit target, int effectValue);
 }
