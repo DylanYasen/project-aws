4ed6123ecccc9adf4c6bcbbed672b595ab5853ef
Decal Bagel <3894957+DylanYasen@users.noreply.github.com>
2024-12-15 12:50:26 -0800

    basic targeting logic

--- Assets/Scripts/UI/CardUI.cs
+++ Assets/Scripts/UI/CardUI.cs
@@ -16,6 +16,9 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
     private RectTransform rectTransform;
     private CanvasGroup canvasGroup;
 
+
+    public bool IsTargeting { get; private set; }
+
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
@@ -78,8 +81,65 @@ public class CardUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, 
         }
         else
         {
-            GameManager.Instance.PlayCard(card);
-            Destroy(gameObject); // Remove card from hand
+            if (card.requiresTarget)
+            {
+                EnterTargetingMode();
+            }
+            else
+            {
+                GameManager.Instance.PlayCard(card);
+
+                // @todo: pool
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void EnterTargetingMode()
+    {
+        IsTargeting = true;
+    }
+
+    public void ExitTargetingMode()
+    {
+        IsTargeting = false;
+        TargetingArrow.Instance.HideArrow();
+    }
+
+    private void Update()
+    {
+        if (IsTargeting)
+        {
+            UpdateTargeting();
+        }
+    }
+
+    public void UpdateTargeting()
+    {
+        Vector3 start = Camera.main.ScreenToWorldPoint(rectTransform.position);
+        start.z = 0;
+
+        Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentMousePosition.z = 0; // Ensure it's on the same plane
+
+        TargetingArrow.Instance.UpdateArrow(start, currentMousePosition);
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            rectTransform.position = originalPosition; // Snap back to original position
+            ExitTargetingMode();
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            // case a ray from mouse position to see if hit enemy
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit.collider != null && hit.collider.CompareTag("Enemy"))
+            {
+                ExitTargetingMode();
+                GameManager.Instance.PlayCard(card, hit.collider.gameObject);
+                Destroy(gameObject);
+            }
         }
     }
 }
