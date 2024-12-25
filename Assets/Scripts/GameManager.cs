decf52e1d8ce1962419b553ef1d50da91f5fb4d2
Decal Bagel <3894957+DylanYasen@users.noreply.github.com>
2024-12-25 13:41:27 -0800

    wire up basic menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,40 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public CombatManager combatManager { get; private set; }
+    public EncounterManager encounterManager { get; private set; }
 
     [Header("UI Prefab")]
     public GameObject combatStatsUIPrefab;
 
-
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         Instance = this;
         combatManager = new CombatManager();
+        encounterManager = new EncounterManager();
+
+        DontDestroyOnLoad(gameObject);
     }
 
-    void Start()
+    public void StartCombatEncounter()
     {
         DeckManager.Instance.Init();
         TurnManager.Instance.Init();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MapScene");
     }
 
     public void EndTurn()
